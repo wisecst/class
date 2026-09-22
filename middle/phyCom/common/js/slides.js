cf3ac2631e245396cd836eb541c78fa6728da8ef
wisecst <ebboyas@gmail.com>
2026-09-22 11:05:25 +0900

    Start PWM autoplay with next key after 100 percent

--- middle/phyCom/common/js/slides.js
+++ middle/phyCom/common/js/slides.js
@@ -43,6 +43,7 @@ document.addEventListener('keydown', async (e)=>{
     if(si===1&&window.lessonCircuit&&window.lessonCircuit.getStep()<3){window.lessonCircuit.next();}
     else if(si===2&&window.entryLesson&&window.entryLesson.getStep()<window.entryLesson.max){window.entryLesson.next();}
     else if(si===3&&window.pwmLesson&&window.pwmLesson.canNext()){window.pwmLesson.next();}
+    else if(si===3&&window.pwmLesson&&window.pwmLesson.isAtMax()){window.pwmLesson.play();}
     else show(si+1);
     return;
   }
